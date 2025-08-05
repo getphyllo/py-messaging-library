436d9a7ca895846b61678f547e10d752f8deec6a
shahaab <shahaab.m@getphyllo.com>
2025-08-05 20:18:11 +0530

    test complete change

--- rabbitmq_client/single_threaded_consumer/single_threaded_multi_queue_listener.py
+++ rabbitmq_client/single_threaded_consumer/single_threaded_multi_queue_listener.py
@@ -2,6 +2,7 @@ import asyncio
 import functools
 import json
 import logging
+import threading
 
 from pika import SelectConnection
 from pika.channel import Channel as PikaChannel
@@ -14,55 +15,71 @@ from rabbitmq_client.consumer.health_utils import set_consumer_status
 from rabbitmq_client.single_threaded_consumer.schemas import ListenQueueConfig
 
 
-class SingleThreadedMultiQueueListener(object):
+class SingleThreadedMultiQueueListener:
     """
-    This is a single threaded multi queue listener, as opposed the other multi queue listener in the library.
-    this listener does not implement threads, which enables us to trigger async methods while handling the messages
+    This is a single-threaded, multi-queue RabbitMQ consumer that supports async message handlers.
+    It avoids threads for message processing, enabling async API calls (like PDF generation).
     """
     def __init__(self, broker_config: BrokerConfig, listen_queue_configs: list[ListenQueueConfig]):
         logging.debug('Creating connection object and registering callbacks')
-        self.connection = get_async_connection(broker_config=broker_config,
-                                               on_connection_open=self.on_connection_open,
-                                               on_connection_closed=self.on_connection_closed)
+        self.connection = get_async_connection(
+            broker_config=broker_config,
+            on_connection_open=self.on_connection_open,
+            on_connection_closed=self.on_connection_closed
+        )
         self.listen_queue_configs = listen_queue_configs
+
+        # Set up a background asyncio event loop for async message handlers
+        self.loop = asyncio.new_event_loop()
+        self.loop_thread = threading.Thread(target=self._start_loop, daemon=True)
+        self.loop_thread.start()
+
+    def _start_loop(self):
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_forever()
 
     def on_connection_open(self, connection: SelectConnection):
         connection.channel(on_open_callback=self.on_channel_open)
 
     def on_connection_closed(self, connection, reason):
         logging.exception('Connection closed: %s', reason)
-        raise Exception('Connection closed: %s', reason)
+        raise Exception(f'Connection closed: {reason}')
 
     def on_channel_open(self, channel: PikaChannel):
         channel.add_on_close_callback(self.on_channel_closed)
 
+        # Set prefetch to 1 to process one message at a time
+        channel.basic_qos(prefetch_count=settings.PREFETCH_COUNT)
+
         def on_message(ch: PikaChannel, method: PikaBasic.Deliver,
                        properties: PikaBasicProperties, body: bytes, args):
-            message = json.loads(body.decode('utf8'))
             (handler,) = args
+            logging.debug("Received message: %s", body)
+            message = json.loads(body.decode('utf8'))
 
             async def handle_and_ack():
+                logging.debug("Starting async handler for delivery_tag=%s", method.delivery_tag)
                 await handler.async_handle_message(
                     method=method, properties=properties, message=message
                 )
                 if ch.is_open:
-                    logging.debug('Channel is open, acknowledging the message')
+                    logging.debug('Acknowledging message with delivery_tag=%s', method.delivery_tag)
                     ch.basic_ack(delivery_tag=method.delivery_tag)
                 else:
-                    logging.warning('Channel closed, unable to ack message')
+                    logging.warning('Channel closed before ack')
 
-            loop = asyncio.get_event_loop()
-            loop.create_task(handle_and_ack())
+            # Submit task to background asyncio loop
+            self.loop.call_soon_threadsafe(lambda: asyncio.ensure_future(handle_and_ack(), loop=self.loop))
 
-        channel.basic_qos(prefetch_count=settings.PREFETCH_COUNT)
         for listen_queue_config in self.listen_queue_configs:
             queue_name: str = listen_queue_config.name
             on_message_callback = functools.partial(on_message, args=(listen_queue_config.handler,))
             channel.basic_consume(queue=queue_name, on_message_callback=on_message_callback, auto_ack=False)
+            logging.info("Consuming from queue: %s", queue_name)
 
     def on_channel_closed(self, ch, reason):
         logging.exception('Channel %i was closed: %s', ch, reason)
-        raise Exception('Channel %i was closed: %s', ch, reason)
+        raise Exception(f'Channel {ch} was closed: {reason}')
 
     def listen(self):
         try:
@@ -76,3 +93,83 @@ class SingleThreadedMultiQueueListener(object):
             set_consumer_status(is_healthy=False)
             self.connection.close()
             raise ex
+
+
+# import asyncio
+# import functools
+# import json
+# import logging
+#
+# from pika import SelectConnection
+# from pika.channel import Channel as PikaChannel
+# from pika.spec import Basic as PikaBasic, BasicProperties as PikaBasicProperties
+#
+# from rabbitmq_client.async_connection.connection import get_async_connection
+# from rabbitmq_client.broker_config import BrokerConfig
+# from rabbitmq_client.config import settings
+# from rabbitmq_client.consumer.health_utils import set_consumer_status
+# from rabbitmq_client.single_threaded_consumer.schemas import ListenQueueConfig
+#
+#
+# class SingleThreadedMultiQueueListener(object):
+#     """
+#     This is a single threaded multi queue listener, as opposed the other multi queue listener in the library.
+#     this listener does not implement threads, which enables us to trigger async methods while handling the messages
+#     """
+#     def __init__(self, broker_config: BrokerConfig, listen_queue_configs: list[ListenQueueConfig]):
+#         logging.debug('Creating connection object and registering callbacks')
+#         self.connection = get_async_connection(broker_config=broker_config,
+#                                                on_connection_open=self.on_connection_open,
+#                                                on_connection_closed=self.on_connection_closed)
+#         self.listen_queue_configs = listen_queue_configs
+#
+#     def on_connection_open(self, connection: SelectConnection):
+#         connection.channel(on_open_callback=self.on_channel_open)
+#
+#     def on_connection_closed(self, connection, reason):
+#         logging.exception('Connection closed: %s', reason)
+#         raise Exception('Connection closed: %s', reason)
+#
+#     def on_channel_open(self, channel: PikaChannel):
+#         channel.add_on_close_callback(self.on_channel_closed)
+#
+#         def on_message(ch: PikaChannel, method: PikaBasic.Deliver,
+#                        properties: PikaBasicProperties, body: bytes, args):
+#             message = json.loads(body.decode('utf8'))
+#             (handler,) = args
+#
+#             async def handle_and_ack():
+#                 await handler.async_handle_message(
+#                     method=method, properties=properties, message=message
+#                 )
+#                 if ch.is_open:
+#                     logging.debug('Channel is open, acknowledging the message')
+#                     ch.basic_ack(delivery_tag=method.delivery_tag)
+#                 else:
+#                     logging.warning('Channel closed, unable to ack message')
+#
+#             loop = asyncio.get_event_loop()
+#             loop.create_task(handle_and_ack())
+#
+#         channel.basic_qos(prefetch_count=settings.PREFETCH_COUNT)
+#         for listen_queue_config in self.listen_queue_configs:
+#             queue_name: str = listen_queue_config.name
+#             on_message_callback = functools.partial(on_message, args=(listen_queue_config.handler,))
+#             channel.basic_consume(queue=queue_name, on_message_callback=on_message_callback, auto_ack=False)
+#
+#     def on_channel_closed(self, ch, reason):
+#         logging.exception('Channel %i was closed: %s', ch, reason)
+#         raise Exception('Channel %i was closed: %s', ch, reason)
+#
+#     def listen(self):
+#         try:
+#             logging.info('Starting IO Loop to listen for messages')
+#             set_consumer_status(is_healthy=True)
+#             self.connection.ioloop.start()
+#         except KeyboardInterrupt:
+#             logging.info('Keyboard Interrupt Closing')
+#             self.connection.close()
+#         except Exception as ex:
+#             set_consumer_status(is_healthy=False)
+#             self.connection.close()
+#             raise ex
